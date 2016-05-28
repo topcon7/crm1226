99355981ab141ee9db05ddb1f23d1a19452871a9
topcon7 <hanyingchao0807@163.com>
2016-05-28 08:53:00 +0800

    77

--- src/com/atguigu/git/test/HelloGit.java
+++ src/com/atguigu/git/test/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
 
 public static void main(String[] args) {
 	System.out.println("hello...");	
-	System.out.println("66666...");	
+	System.out.println("66666...");
+	System.out.println("77777...");
 }
 }
